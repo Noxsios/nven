beecfaff19148c4beab6f4a6a510c290f30ba14b
razzle <harry@razzle.cloud>
2022-10-04 02:17:55 -0500

    init mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 import { SessionProvider } from "next-auth/react";
 
 // @ts-ignore
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "light",
+      }}
+    >
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from "next";
-import LoginBtn from "../components/login-btn";
+import { AppShell, Navbar, Header, Button } from "@mantine/core";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   return (
-    <div>
-      <LoginBtn />
-    </div>
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          {/* Navbar content */}
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          {/* Header content */}
+          {session ? <Button onClick={() => signOut()}>Sign out</Button> : <Button onClick={() => signIn()}>Sign in</Button>}
+        </Header>
+      }
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      <pre>
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
+    </AppShell>
   );
 };
 
