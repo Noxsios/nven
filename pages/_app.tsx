b045ff201df681a8af3877c8df3f7a1225c9af31
razzle <harry@razzle.cloud>
2022-10-04 02:44:51 -0500

    init appshell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
 import "../styles/globals.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from "@mantine/core";
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 
 // @ts-ignore
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        /** Put your mantine theme override here */
-        colorScheme: "light",
-      }}
-    >
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
-import { AppShell, Navbar, Header, Button } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons";
+import { AppShell, Navbar, Header, Button, Group, ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Logo } from "../components/logo";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
   return (
     <AppShell
       padding="md"
@@ -14,8 +18,15 @@ const Home: NextPage = () => {
       }
       header={
         <Header height={60} p="xs">
-          {/* Header content */}
-          {session ? <Button onClick={() => signOut()}>Sign out</Button> : <Button onClick={() => signIn()}>Sign in</Button>}
+          <Group sx={{ height: "100%" }} px={20} position="apart">
+            <Logo colorScheme={colorScheme} />
+            <Group>
+              <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+                {colorScheme === "dark" ? <IconSun size={16} /> : <IconMoonStars size={16} />}
+              </ActionIcon>
+              {session ? <Button onClick={() => signOut()}>Sign Out</Button> : <Button onClick={() => signIn()}>Sign In</Button>}
+            </Group>
+          </Group>
         </Header>
       }
       styles={(theme) => ({
